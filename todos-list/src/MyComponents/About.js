cfdb9d665bcbb09e21d3e5db5092044dc8d2f74b
Ashwani1806 <ashwanishrivastav20084@gmail.com>
2023-07-27 18:25:23 +0530

    Initial commit

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>In this website you can add and delete your daily todo list on daily basis work.</p>
+    </div>
+  )
+}
+
